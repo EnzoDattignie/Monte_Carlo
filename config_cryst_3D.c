5900d8660d566d5035e02d7bdb66cf9b3a3ba840
EnzoDattignie <enzo.dattignie@gmail.com>
2025-11-30 18:36:39 +0100

    Implémentation MC, création des output, verif 2D et 3D

--- config_cryst_3D.c
+++ config_cryst_3D.c
@@ -3,8 +3,8 @@
 #include <math.h>
 
 int main (int argc, char *argv[]) {
-    int n = 10; //N = n*n
-    double box_size = 10., posy, posx;
+    int n = 6; //N = n*n*n
+    double box_size = 20., posy, posx, posz;
     if (argc == 3) {
         sscanf(argv[1],"%d",&n);
         sscanf(argv[2],"%lf",&box_size);
@@ -13,14 +13,18 @@ int main (int argc, char *argv[]) {
     posx = (-box_size+dx)/2;
     FILE *file;
 
-    file = fopen("cryst2D.xyz","w");
-    fprintf(file,"%d\n%lf %lf\n",n*n, box_size, box_size);
+    file = fopen("cryst3D.xyz","w");
+    fprintf(file,"%d\n%lf %lf %lf\n",n*n*n, box_size, box_size, box_size);
 
-
+    
     for (int l = 0; l < n; l++) {
         posy = (-box_size+dx)/2 ;
         for (int c = 0; c < n; c++) {
-            fprintf(file,"A %f %f\n",posx, posy);
+            posz = (-box_size+dx)/2 ;
+            for (int z = 0; z < n; z++) {
+                fprintf(file,"A %f %f %f\n",posx, posy, posz);
+                posz += dx;
+            }
             posy += dx;
         }
         posx += dx;
